8a0345d69595ad7b32832c97abd9b66f1a9bd022
rabon-z <1248597683@qq.com>
2020-07-06 20:44:06 +0800

    homework1

---
+++ homework1/app/src/main/java/com/example/homework1/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.homework1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+};
+
